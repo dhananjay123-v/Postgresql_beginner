90a1c61d3003f9bab7d39fa72ac7c55d1513d90d
Dhananjay Vishwakarma <mrstrange372@gmail.com>
2025-06-23 21:09:08 +0530

    Update topskill_for_myRole.sql

--- sql_project/topskill_for_myRole.sql
+++ sql_project/topskill_for_myRole.sql
@@ -1,6 +1,7 @@
 /*
+Question: 
 
-Question: What are the most in-demand skills for data analysts?
+What are the most in-demand skills for data analysts?
 Join job postings to inner join table similar to query 2
 Identify the top 5 in-demand skills for a data analyst.
 Focus on all job postings.
